520544a77fb00ac63edc35b20cc322aa07c5608a
nai <nai@nai.com>
2022-05-31 22:05:55 -0400

    Stage 3 complete

--- SunnyMood/Assets/PlayerScript.cs
+++ SunnyMood/Assets/PlayerScript.cs
@@ -3,20 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public class PlayerScript : MonoBehaviour
 {
     public Rigidbody2D rb;
-    private new BoxCollider2D _collider;
+
+    public SpriteRenderer sr;
+    //[FormerlySerializedAs("_collider")] public BoxCollider2D collider;
     private float speed = 0.0f;
     private bool isJumpPressed;
     private bool isGrounded = true;
-    
+    public Animator animator;
+    private static readonly int Speed = Animator.StringToHash("Speed");
+    private static readonly int IsJumping = Animator.StringToHash("isJumping");
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        _collider = GetComponent<BoxCollider2D>();
+        //rb = GetComponent<Rigidbody2D>();
+        //_collider = GetComponent<BoxCollider2D>();
+        
     }
 
     // Update is called once per frame
@@ -30,6 +37,7 @@ public class PlayerScript : MonoBehaviour
         {
             //print("jump");
             isGrounded = false;
+            animator.SetBool(IsJumping, !isGrounded);
             rb.AddForce(new Vector2(0, 300));
             transform.Translate(0, 0.1f, 0);
             //rb.velocity = new Vector2(0, 5);
@@ -40,6 +48,16 @@ public class PlayerScript : MonoBehaviour
     {
         speed = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
         rb.velocity = new Vector2(speed * 200, rb.velocity.y);
+        animator.SetFloat(Speed, Mathf.Abs(speed*200));
+
+        if (speed < 0)
+        {
+            sr.flipX = true;
+        }
+        else if (speed > 0)
+        {
+            sr.flipX = false;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -48,7 +66,12 @@ public class PlayerScript : MonoBehaviour
         {
             //print("Ground");
             isGrounded = collision.collider.bounds.min.y < transform.position.y;
+            animator.SetBool(IsJumping, !isGrounded);
             //print(isGrounded);
+        }
+        else if (collision.collider.CompareTag("Gem"))
+        {
+            //Destroy(collision.gameObject);
         }
         
     }
