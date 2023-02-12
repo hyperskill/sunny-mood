d9590b2eb87ff4db09646a93918d5f23116ef76f
user <user@home>
2023-02-12 03:29:28 +0200

    I can't pass the tests

---
+++ SunnyMood/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Transform objToFollow;
+
+    private Vector3 deltaPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        deltaPos = transform.position = objToFollow.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = objToFollow.position + deltaPos;
+    }
+}
---
+++ SunnyMood/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D r;
+    private bool playerOnTheJump = false;
+    private bool spacePressed = false;
+    private float movementSpeed = 10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            spacePressed = true;
+        if(Input.GetKeyUp(KeyCode.Space))
+            spacePressed = false;
+        if (spacePressed&&!playerOnTheJump)
+        {
+            playerOnTheJump = true;
+            r.AddForce(new Vector2(0, 300));
+        }
+        
+    }
+    private void FixedUpdate()
+    {
+        float speed = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+        r.velocity = new Vector2(speed * 500, r.velocity.y);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.name == "Ground")
+        {
+            if(collision.collider.bounds.min.y < transform.position.y)
+                playerOnTheJump = false;
+        }
+    }
+}
